ee003aa5a400bbef8aace0acf57be5f6abcb8693
ksu_smolyar <ksu_smolyar@mail.ru>
2025-02-12 14:09:51 +0400

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-arrow-game/",
   plugins: [react()],
   server: {
     open: true,
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
   },
   test: {
     globals: true,
